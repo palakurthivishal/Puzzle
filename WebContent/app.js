60ae0cc381a893714dc5a9183b3a229403b7e46d
palakurthivishal <palakurthivishal@gmail.com>
2014-04-21 18:33:54 +0530

    Puzzle repo

---
+++ WebContent/app.js
@@ -0,0 +1,12 @@
+Ext.application({
+	name : 'ExtSample',
+	controllers : ['PuzzleController'],
+	views : ['Puzzle','PuzzlePiece', 'Chessboard'],
+	autoCreateViewport : true,
+	launch : function(){
+		
+		
+	}
+	
+}
+);
